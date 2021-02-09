c45235b1135f3d33c219a667f4f5d29a88815c21
Bastien Berardi <b.berardi@criteo.com>
2021-02-09 08:46:13 +0100

    delete

--- cours.js
+++
@@ -1,8 +0,0 @@
-$(document.body).ready(function () {
-  $(document).on("click", "#MonHeader", function (event) {
-    $("#PopUp").html("<span style='color:red'>mon text</span>");
-    $("#PopUp").attr("style", "display:block !important");
-
-    // $("#Colonne1").css("backgroundColor", "#1CEA50");
-  });
-});
