e961bbe5f971b1e0de95c0f5a8ef6011f1089f9e
Lucianalopes1013 <71490718+Lucianalopes1013@users.noreply.github.com>
2021-02-20 18:19:02 +0100

    update contact

--- contact.php
+++ contact.php
@@ -20,17 +20,28 @@
     <div id="Content" class="p-4">
       <form>
         <div class="form-group">
+         <label for="exampleInputPrenom">First Name </label>
+          <input type="First Name" class="form-control" id="exampleInputFirstName" aria-describedby="" placeholder="Insert here your first name">    </div>
+            <div class="form-group">
+         <label for="exampleInputNom">Last name </label>
+          <input type="Last name" class="form-control" id="exampleInputLastName" aria-describedby="" placeholder="Insert here your last name">    </div>
+           <div class="form-group">
           <label for="exampleInputEmail1">Email address</label>
           <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
           <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        </div>
+         <div class="form-group">
+          <label for="Subject">Subject</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Tell us the subject of your message">
+          </div>
+           <div class="form-group">
+          <label for="Message">Message</label>
+          <textarea id="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Tell us the subject of your message">
+          </div>
+   
         <div class="form-check">
           <input type="checkbox" class="form-check-input" id="exampleCheck1">
-          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>            
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
