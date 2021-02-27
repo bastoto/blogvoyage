f8712eab5b4a4a4c316f7b6b21742e671eea897b
Lucianalopes1013 <71490718+Lucianalopes1013@users.noreply.github.com>
2021-02-27 19:11:19 +0100

    bdd

--- PHP/bdd.php
+++ PHP/bdd.php
@@ -1,7 +1,7 @@
 <?php
-$server = "";
-$name_user = "";
-$name_database = "";
+$server = "localhost"; //* l'adresse de mon serveur
+$name_user = "root"; 
+$name_database = "pocketmemories"; //* nom de ma BDD
 $password = "";
 $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
 $bdd = new PDO("mysql:host=$server;dbname=$name_database", "$name_user", "$password", $pdo_options);
