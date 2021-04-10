c55957abceb81037b304dde37cc6066779f5cbd4
Bastien Berardi <b.berardi@criteo.com>
2021-04-10 17:05:03 +0200

    new version site
    fixé les animation images

--- PHP/home.php
+++ PHP/home.php
@@ -1,25 +1,33 @@
-<div id="Colonne1" class="inblock">
+<div class="row">
+    <div class="col-8 inblock">
         <ol>
-          <li>
-            Travelling Alone...why do I think that is one of the best feeling in
-            the world?
-          </li>
-          <li>My honest opinion on my first solo trip</li>
-          <li>The heartbreaker, New York city</li>
-          <li>Travelling outside European Union, what to expect?</li>
-          <li>Italy, il mio eterne amore</li>
+            <li>
+                Travelling Alone...why do I think that is one of the best feeling in
+                the world?
+            </li>
+            <li>My honest opinion on my first solo trip</li>
+            <li>The heartbreaker, New York city</li>
+            <li>Travelling outside European Union, what to expect?</li>
+            <li>Italy, il mio eterne amore</li>
         </ol>
-      </div>
-      <div id="Colonne2" class="inblock"></div>
+    </div>
+    <div class="col-4 inblock"></div>
+</div>
+
+<div id="TOPDESTINATIONS" class="text-center">TOP DESTINATIONS</div>
+<div class="row">
+    <div class="inblock titreAligne col-4 p-2">
+        <div id="Colonne3" class="ColonneStyle"> <div>ITALY</div></div>
+
+    </div>
+    <div class="inblock titreAligne  col-4 p-2">
+        <div id="Colonne4" class="ColonneStyle">  <div>RUSSIE</div></div>
+
+    </div>
+    <div class="inblock titreAligne  col-4 p-2">
+        <div id="Colonne5" class="ColonneStyle"><div>UK</div></div>
+
+    </div>
+</div>
 
 
-<div id="TOPDESTINATIONS" class="Themes">TOP DESTINATIONS</div>
-<div id="Colonne3" class="inblock titreAligne ColonneStyle">
-    ITALY
-</div>
-<div id="Colonne4" class="inblock titreAligne ColonneStyle">
-    RUSSIE
-</div>
-<div id="Colonne5" class="inblock titreAligne ColonneStyle">
-    LONDON
-</div>
